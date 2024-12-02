73d3eebae85e877b8f039e3657c7ca4937d8ceab
Ajay Chawada <ajay@Ajays-MacBook-Air.local>
2024-12-02 08:54:00 +0530

    Removed duplicated function

--- src/screen/Desktop/Home/Home.js
+++ src/screen/Desktop/Home/Home.js
@@ -1,24 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Menubar from "../../../components/common/Menubar/Menubar";
 
 function Home() {
-  let container;
-
-  useEffect(() => {
-    container = document.getElementById("mac-container");
-    container.addEventListener("mousemove", handleMouseMove);
-
-    return () => {
-      if (container) {
-        container.removeEventListener("mousemove", handleMouseMove);
-      }
-    };
-  }, []);
-
-  const handleMouseMove = (event) => {
-    console.log(`Cursor Position: X=${event.clientX}, Y=${event.clientY}`);
-  };
-
   return (
     <div className="min-w-[100vw]">
       <Menubar fixedMenubar={true} />
